6edb270862630f1f76fd027c834340e2b99c52e0
thinking <1294873004@qq.com>
2020-02-25 22:19:15 +0800

    init wepy src plugin

---
+++ index.js
@@ -0,0 +1,22 @@
+const ImageExtReg = /\.(gif|jpg|jpeg|tiff|png)$/;
+const HadSetReg = /^(https?|\/|\d{1,3})/;
+exports = module.exports = function wepyImageSrcPlugin(options) {
+  let baseImageUrl = options.baseUrl || "/";
+  const ext = options.ext || "png";
+
+  return function() {
+    this.register("template-parse-ast-pre-attr-src", function(config) {
+      let { expr } = config;
+      let parsed = this.hookUnique("url-to-module", expr);
+      if (!parsed.isModule && !HadSetReg.test(expr)) {
+        if (!ImageExtReg.test(expr)) {
+          expr = `${expr}.${ext}`;
+        }
+        expr = `${baseImageUrl}${expr}`;
+        config.expr = expr;
+      }
+
+      return config;
+    });
+  };
+};
