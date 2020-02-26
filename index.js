f2fb8789171ec13ef1530895454af41b48430662
thinking <1294873004@qq.com>
2020-02-26 21:13:43 +0800

    support icon

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const ImageExtReg = /\.(gif|jpg|jpeg|tiff|png)$/;
 const HadSetReg = /^(https?|\/|\d{1,3})/;
+const attr = ["src", "icon"];
+
 exports = module.exports = function wepyImageSrcPlugin(options) {
   let baseImageUrl = options.baseUrl || "/";
   const ext = options.ext || "png";
 
   return function() {
-    this.register("template-parse-ast-pre-attr-src", function(config) {
-      let { expr } = config;
-      let parsed = this.hookUnique("url-to-module", expr);
-      if (!parsed.isModule && !HadSetReg.test(expr)) {
-        if (!ImageExtReg.test(expr)) {
-          expr = `${expr}.${ext}`;
+    attr.forEach(t => {
+      this.register(`template-parse-ast-pre-attr-${t}`, function prefixSrc(
+        config
+      ) {
+        let { expr } = config;
+        let parsed = this.hookUnique("url-to-module", expr);
+        if (!parsed.isModule && !HadSetReg.test(expr)) {
+          if (!ImageExtReg.test(expr)) {
+            expr = `${expr}.${ext}`;
+          }
+          expr = `${baseImageUrl}${expr}`;
+          config.expr = expr;
         }
-        expr = `${baseImageUrl}${expr}`;
-        config.expr = expr;
-      }
 
-      return config;
+        return config;
+      });
     });
   };
 };
--- main.js
+++ main.js
@@ -2,25 +2,30 @@
 
 var ImageExtReg = /\.(gif|jpg|jpeg|tiff|png)$/;
 var HadSetReg = /^(https?|\/|\d{1,3})/;
+var attr = ["src", "icon"];
 
 exports = module.exports = function wepyImageSrcPlugin(options) {
   var baseImageUrl = options.baseUrl || "/";
   var ext = options.ext || "png";
   return function () {
-    this.register("template-parse-ast-pre-attr-src", function (config) {
-      var expr = config.expr;
-      var parsed = this.hookUnique("url-to-module", expr);
+    var _this = this;
 
-      if (!parsed.isModule && !HadSetReg.test(expr)) {
-        if (!ImageExtReg.test(expr)) {
-          expr = "".concat(expr, ".").concat(ext);
+    attr.forEach(function (t) {
+      _this.register("template-parse-ast-pre-attr-".concat(t), function prefixSrc(config) {
+        var expr = config.expr;
+        var parsed = this.hookUnique("url-to-module", expr);
+
+        if (!parsed.isModule && !HadSetReg.test(expr)) {
+          if (!ImageExtReg.test(expr)) {
+            expr = "".concat(expr, ".").concat(ext);
+          }
+
+          expr = "".concat(baseImageUrl).concat(expr);
+          config.expr = expr;
         }
 
-        expr = "".concat(baseImageUrl).concat(expr);
-        config.expr = expr;
-      }
-
-      return config;
+        return config;
+      });
     });
   };
 };
